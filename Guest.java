9c33ce56ff8a2d081a9cf336acff9f2f9300c775
MichaelMet <mikegmac14@gmail.com>
2023-10-25 15:06:00 -0700

    Added a makeReservation and cancelReservation method

--- Guest.java
+++ Guest.java
@@ -14,9 +14,22 @@ public String getFirstName(){
     return firstName;
 }
 
-public void makeReservation(Date){
-    
+//makes a reservation
+//for date it uses the Java.time package
+// changes can be made depending on future code changes
+public Reservation makeReservation(LocalDatetime startDate, LocalDatetime endDate, HotelRoom room,Guest guest){
+    Reservation newReservation = new Resercation(startDate, endDate, room, guest);
+    room.reserve();
+    return newReservation;
+}
 
+//calles a method in HotelRoom to unreserve the room
+public void cancelReservation(Reservation reservation){
+    reservation.room.unreserve();
+}
+
+public void sendEmail(String email){
+// to be added
 }
 
 
